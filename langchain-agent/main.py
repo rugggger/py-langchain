11b60ae68b9b0364eee97380ed61ae68a234d748
Yaron Bar <bar.yaron2018@gmail.com>
2024-07-27 22:35:39 +0300

    ai tool

--- langchain-agent/main.py
+++ langchain-agent/main.py
@@ -2,25 +2,36 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
-    MessagesPlaceholder
+    MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+print(tables)
+
 prompt = ChatPromptTemplate(
     messages=[
-        HumanMessagePromptTemplate().from_template("{input}"),
+        SystemMessage(
+            content=(
+                "You are an AI that can access a sqlite database.\n"
+                f"The database has tables of: {tables}\n"
+                "Do not make assumptions about what tables exist or what columns exist "
+                "Instead, use the 'describe_tables' function"
+            )),
+        HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 agent = OpenAIFunctionsAgent(
     llm=chat,
     prompt=prompt,
@@ -33,6 +44,6 @@ agent_executor = AgentExecutor(
     tools=tools,
 )
 
-agent_executor("How many users are in the database?")
+agent_executor("Tell me the name and address of the user that ordered the most orders")
 
-print("g")
+
