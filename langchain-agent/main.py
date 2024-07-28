0a233eb7463a2a9010bdbc79f83b00e416139696
Yaron Bar <bar.yaron2018@gmail.com>
2024-07-28 09:34:18 +0300

    memory

--- langchain-agent/main.py
+++ langchain-agent/main.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 
@@ -27,11 +28,16 @@ prompt = ChatPromptTemplate(
                 "Do not make assumptions about what tables exist or what columns exist "
                 "Instead, use the 'describe_tables' function"
             )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -43,9 +49,11 @@ agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
     tools=tools,
+    memory=memory
 )
-#agent_executor("Summarize the top 5 most popular products. Write the results to a report file.")
+
 
 agent_executor("Prepare a report of for the user that ordered the most orders. Write the results to a report file")
+agent_executor("Repeat the same for users.")
 
 
