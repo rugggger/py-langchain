be291490760c445e22cebe9aae0180247847dbac
Yaron Bar <bar.yaron2018@gmail.com>
2024-07-28 10:00:46 +0300

    logs messages

--- langchain-agent/handlers/chat_model_start_handler.py
+++ langchain-agent/handlers/chat_model_start_handler.py
@@ -1,11 +1,28 @@
-from typing import Dict, Any, List, Optional
-from uuid import UUID
-
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain_core.messages import ChatMessage, BaseMessage
+from pyboxen import boxen
 
+def boxen_print(*args, **kwargs):
+    print(boxen(*args, **kwargs))
 
+boxen_print("tet this ",title="test", color="red")
 class ChatModelStartHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized, messages, **kwargs):
-        print(messages)
+        print("\n\n\n\n========= Sending messages ========= \n\n ")
+        for message in messages[0]:
+            if message.type == "system":
+                boxen_print(message.content, title="system", color="yellow")
+
+            elif message.type == "human":
+                boxen_print(message.content, title="human", color="blue")
+
+            elif message.type == "ai" and "function_call" in message.additional_kwargs:
+                call = message.additional_kwargs["function_call"]
+                boxen_print(f"running tool {call['name']}", title=message.type, color="cyan")
+
+            elif message.type == "ai":
+                boxen_print(message.content, title="ai", color="green")
+
+            elif message.type == "function":
+                boxen_print(message.content, title="ai", color="purple")
+
 
